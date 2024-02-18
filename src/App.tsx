6c481afbc8370d49f36ac621ca68ef3457719176
AliUmed <aliumed844@gmail.com>
2024-02-19 01:18:00 +0300

    add Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,39 @@
 import "tailwindcss/tailwind.css";
+import Button from "./component/Button";
 
 function App() {
+  const calculatorData = [
+    "c",
+    "+/-",
+    "%",
+    "/",
+    "7",
+    "8",
+    "9",
+    "x",
+    "4",
+    "5",
+    "6",
+    "-",
+    "1",
+    "2",
+    "3",
+    "+",
+    "0",
+    ".",
+    "=",
+  ];
   return (
     <div className="w-80 h-auto  mx-auto bg-gray-100 p-4 rounded-lg shadow-md">
       <div className="bg-white rounded-lg p-2 mb-4">123</div>
 
       <div className=" grid grid-cols-4 space-y-3">
-        <Button text={"c"} />
-        <Button text={"+/-"} />
-        <Button text={"%"} />
-        <Button text={"/"} />
-        <Button text={"7"} />
-        <Button text={"8"} />
-        <Button text={"9"} />
-        <Button text={"x"} />
-        <Button text={"4"} />
-        <Button text={"5"} />
-        <Button text={"6"} />
-        <Button text={"-"} />
-        <Button text={"1"} />
-        <Button text={"2"} />
-        <Button text={"3"} />
-        <Button text={"+"} />
-        <Button text={"0"} />
-        <Button text={"."} />
-        <Button text={"="} />
+        {calculatorData.map((item, index) => (
+          <Button text={item} key={index} />
+        ))}
       </div>
     </div>
   );
 }
 
-function Button({ text }: { text: string }) {
-  return <button className=" rounded-full bg-gray-400 size-10">{text}</button>;
-}
 export default App;
---
+++ src/component/Button.tsx
@@ -0,0 +1,3 @@
+export default function Button({ text }: { text: string }) {
+  return <button className=" rounded-full bg-gray-400 size-10">{text}</button>;
+}
