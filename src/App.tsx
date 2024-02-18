b4eeaf7fe70a0527b18f09f57b15c56ec11008ab
AliUmed <aliumed844@gmail.com>
2024-02-19 02:12:30 +0300

    add object to array contains text and color now color for test

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,34 @@ import Button from "./component/Button";
 
 function App() {
   const calculatorData = [
-    "c",
-    "+/-",
-    "%",
-    "/",
-    "7",
-    "8",
-    "9",
-    "x",
-    "4",
-    "5",
-    "6",
-    "-",
-    "1",
-    "2",
-    "3",
-    "+",
-    "0",
-    ".",
-    "=",
+    { text: "c", color: "red" },
+    { text: "+/-", color: "blue" },
+    { text: "%", color: "green" },
+    { text: "/", color: "yellow" },
+    { text: "7", color: "purple" },
+    { text: "8", color: "orange" },
+    { text: "9", color: "pink" },
+    { text: "x", color: "teal" },
+    { text: "4", color: "indigo" },
+    { text: "5", color: "gray" },
+    { text: "6", color: "cyan" },
+    { text: "-", color: "lime" },
+    { text: "1", color: "purple" },
+    { text: "2", color: "rose" },
+    { text: "3", color: "emerald" },
+    { text: "+", color: "violet" },
+    { text: "0", color: "amber" },
+    { text: ".", color: "teal" },
+    { text: "=", color: "orange" },
   ];
+
   return (
-    <div className="w-80 h-auto  mx-auto bg-gray-100 p-4 rounded-lg shadow-md">
+    <div className="w-80 h-auto mx-auto bg-gray-100 p-4 rounded-lg shadow-md">
       <div className="bg-white rounded-lg p-2 mb-4">123</div>
 
-      <div className=" grid grid-cols-4 space-y-3">
+      <div className="grid grid-cols-4 gap-3">
         {calculatorData.map((item, index) => (
-          <Button text={item} key={index} />
+          <Button text={item.text} color={item.color} key={index} />
         ))}
       </div>
     </div>
--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -1,3 +1,17 @@
-export default function Button({ text }: { text: string }) {
-  return <button className=" rounded-full bg-gray-400 size-10">{text}</button>;
+export default function Button({
+  text,
+  color,
+}: {
+  text: string;
+  color: string;
+}) {
+  return (
+    <button
+      className={`rounded-full bg-${color}-500  bg-warmGray-500 ${
+        text === "0" ? "col-span-2 w-32 h-11" : "w-11 h-11"
+      } row-span-2`}
+    >
+      {text}
+    </button>
+  );
 }
