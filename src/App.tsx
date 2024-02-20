10317350d51d16dc1403d272fe2656a979a3c4f0
Ali-Umed <aliumed844@gmail.com>
2024-02-20 18:53:55 +0300

    fix color by prop

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,30 @@ import Button from "./component/Button";
 
 function App() {
   const calculatorData = [
-    { text: "c", color: "red" },
-    { text: "+/-", color: "blue" },
-    { text: "%", color: "green" },
-    { text: "/", color: "yellow" },
-    { text: "7", color: "purple" },
-    { text: "8", color: "orange" },
-    { text: "9", color: "pink" },
-    { text: "x", color: "teal" },
-    { text: "4", color: "indigo" },
-    { text: "5", color: "gray" },
-    { text: "6", color: "cyan" },
-    { text: "-", color: "lime" },
-    { text: "1", color: "purple" },
-    { text: "2", color: "rose" },
-    { text: "3", color: "emerald" },
-    { text: "+", color: "violet" },
-    { text: "0", color: "amber" },
-    { text: ".", color: "teal" },
-    { text: "=", color: "orange" },
+    { text: "c", color: "bg-red-500" },
+    { text: "+/-", color: "bg-blue-500" },
+    { text: "%", color: "bg-green-500" },
+    { text: "/", color: "bg-yellow-500" },
+    { text: "7", color: "bg-purple-500" },
+    { text: "8", color: "bg-orange-500" },
+    { text: "9", color: "bg-pink-500" },
+    { text: "x", color: "bg-teal-500" },
+    { text: "4", color: "bg-indigo-500" },
+    { text: "5", color: "bg-gray-500" },
+    { text: "6", color: "bg-cyan-500" },
+    { text: "-", color: "bg-lime-500" },
+    { text: "1", color: "bg-purple-500" },
+    { text: "2", color: "bg-rose-500" },
+    { text: "3", color: "bg-emerald-500" },
+    { text: "+", color: "bg-violet-500" },
+    { text: "0", color: "bg-amber-500" },
+    { text: ".", color: "bg-teal-500" },
+    { text: "=", color: "bg-orange-500" },
   ];
 
   return (
-    <div className="w-80 h-auto mx-auto bg-gray-100 p-4 rounded-lg shadow-md">
-      <div className="bg-white rounded-lg p-2 mb-4">123</div>
+    <div className="w-80 h-auto mx-auto bg-gray-100 p-5 rounded-lg shadow-md">
+      <div className="bg-white rounded-lg p-2 mb-6 text-right pr-4">123</div>
 
       <div className="grid grid-cols-4 gap-3">
         {calculatorData.map((item, index) => (
--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -5,9 +5,10 @@ export default function Button({
   text: string;
   color: string;
 }) {
+  // console.log(`bg-${color}-500 `);
   return (
     <button
-      className={`rounded-full bg-${color}-500  bg-warmGray-500 ${
+      className={` ${color}  rounded-full   ${
         text === "0" ? "col-span-2 w-32 h-11" : "w-11 h-11"
       } row-span-2`}
     >
