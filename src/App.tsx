3e0026d237ce1cb67d6a445696113fab04643a2f
Ali-Umed <aliumed844@gmail.com>
2024-02-20 20:51:27 +0300

    change color and app to center

--- src/App.tsx
+++ src/App.tsx
@@ -25,35 +25,37 @@ function App() {
   // ];
 
   const calculatorData = [
-    { text: "c", color: "bg-lime-500" },
-    { text: "+/-", color: "bg-lime-500" },
-    { text: "%", color: "bg-lime-500" },
-    { text: "/", color: "bg-lime-500" },
-    { text: "7", color: "bg-lime-500" },
-    { text: "8", color: "bg-lime-500" },
-    { text: "9", color: "bg-lime-500" },
-    { text: "x", color: "bg-lime-500" },
-    { text: "4", color: "bg-lime-500" },
-    { text: "5", color: "bg-lime-500" },
-    { text: "6", color: "bg-lime-500" },
-    { text: "-", color: "bg-lime-500" },
-    { text: "1", color: "bg-lime-500" },
-    { text: "2", color: "bg-lime-500" },
-    { text: "3", color: "bg-lime-500" },
-    { text: "+", color: "bg-lime-500" },
-    { text: "0", color: "bg-lime-500" },
-    { text: ".", color: "bg-lime-500" },
-    { text: "=", color: "bg-lime-500" },
+    { text: "c", color: "bg-gray-300" },
+    { text: "+/-", color: "bg-gray-300" },
+    { text: "%", color: "bg-gray-300" },
+    { text: "/", color: "bg-orange-400" },
+    { text: "7", color: "bg-sky-200" },
+    { text: "8", color: "bg-sky-200" },
+    { text: "9", color: "bg-sky-200" },
+    { text: "x", color: "bg-orange-400" },
+    { text: "4", color: "bg-sky-200" },
+    { text: "5", color: "bg-sky-200" },
+    { text: "6", color: "bg-sky-200" },
+    { text: "-", color: "bg-orange-400" },
+    { text: "1", color: "bg-sky-200" },
+    { text: "2", color: "bg-sky-200" },
+    { text: "3", color: "bg-sky-200" },
+    { text: "+", color: "bg-orange-400" },
+    { text: "0", color: "bg-sky-200" },
+    { text: ".", color: "bg-sky-200" },
+    { text: "=", color: "bg-orange-400" },
   ];
 
   return (
-    <div className="w-80 h-auto mx-auto bg-gray-100 p-5 rounded-lg shadow-md">
-      <div className="bg-white rounded-lg p-2 mb-6 text-right pr-4">123</div>
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-80 h-auto mx-auto bg-gray-100 p-5 rounded-lg shadow-inner shadow-slate-300 ">
+        <div className="bg-white rounded-lg p-2 mb-6 text-right pr-4">123</div>
 
-      <div className="grid grid-cols-4 gap-3">
-        {calculatorData.map((item, index) => (
-          <Button text={item.text} color={item.color} key={index} />
-        ))}
+        <div className="grid grid-cols-4 gap-3">
+          {calculatorData.map((item, index) => (
+            <Button text={item.text} color={item.color} key={index} />
+          ))}
+        </div>
       </div>
     </div>
   );
