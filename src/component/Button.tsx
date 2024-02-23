e0ce3dfe5cf11c36f75a1c932af553cc4e1eba9b
Ali-Umed <aliumed844@gmail.com>
2024-02-23 17:14:20 +0300

    feture: add icondelete insteand on d word

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -1,3 +1,5 @@
+import { FiDelete } from "react-icons/fi";
+
 export function Button({
   text,
   color,
@@ -14,11 +16,11 @@ export function Button({
   return (
     <button
       onClick={() => onClick()}
-      className={`${color} rounded-full ${
+      className={`${color} rounded-full text-center flex justify-center items-center ${
         text === "00" ? "col-span-2 w-32 h-11" : "w-11 h-11"
       } row-span-2`}
     >
-      {text}
+      {text === "d" ? <FiDelete /> : text}
     </button>
   );
 }
