5a81084e2b57524e4740c0ca8232498bf0c7413c
Ali-Umed <aliumed844@gmail.com>
2024-02-24 21:26:47 +0300

    add transition

--- src/App.tsx
+++ src/App.tsx
@@ -183,7 +183,7 @@ export default function App() {
 
   return (
     <div
-      className={`flex flex-col justify-center items-center h-screen ${
+      className={`flex flex-col justify-center items-center h-screen transition-all duration-500  ${
         darkMode ? "bg-[#201e1e]" : "bg-teal-50"
       }`}
     >
@@ -212,7 +212,7 @@ export default function App() {
       </div>
 
       <div
-        className={`w-80 h-auto p-5 rounded-lg shadow-inner shadow-slate-400 ${theme.bgColor} `}
+        className={`w-80 h-auto p-5 rounded-lg shadow-inner shadow-slate-400 ${theme.bgColor} transition-all duration-500 `}
       >
         <div className="bg-white rounded-lg p-2 mb-6 text-right pr-4 border-2 border-gray">
           {output}
