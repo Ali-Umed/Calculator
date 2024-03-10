975da77640bc6520642e9239a65f51008d742a54
Ali-Umed <aliumed844@gmail.com>
2024-03-10 17:12:21 +0300

    fix isuue 0/0 now undifind

--- src/component/CalculatorOutput.tsx
+++ src/component/CalculatorOutput.tsx
@@ -1,7 +1,9 @@
 export default function CalculatorOutput({ output }: { output: string }) {
   return (
     <div className="bg-white rounded-lg p-4 mb-6 text-right border border-gray-300">
-      <p className="text-xl font-bold">{output}</p>
+      <p className="text-xl font-bold">
+        {output == "NaN" ? "undifind" : output}
+      </p>
     </div>
   );
 }
